eeb296129950dc276402e7f0520936d1528d9642
avitrost <avitrost@gmail.com>
2025-04-24 15:00:58 -0500

    add logging

--- verifiers/rubrics/math_verify_rubric.py
+++ verifiers/rubrics/math_verify_rubric.py
@@ -1,11 +1,16 @@
 from verifiers.rubrics import Rubric
+from typing import List
 
 
 class MathVerifyRubric(Rubric):
     def __init__(self):
         from verifiers.rubrics import is_latex_equal
+
+        def latex_equal_reward_func(completions, answer, **kwargs) -> List[float | None]:
+            return [1.0 if is_latex_equal(r, a) else 0.0 for r, a in zip(completions, answer)]
+
         self.reward_funcs = [
-            is_latex_equal
+            latex_equal_reward_func
         ]
         self.reward_weights = [
             1
