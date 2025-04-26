3936e64f657228c1c4e4fd2aa726361669ffe1e4
avitrost <avitrost@gmail.com>
2025-04-26 13:18:02 -0500

    add logging

--- verifiers/envs/scratchpad_env.py
+++ verifiers/envs/scratchpad_env.py
@@ -121,9 +121,11 @@ class ScratchpadEnv(Environment):
         return count
     
     def is_completed(self, messages: List[Dict[str, str]], answer: str, **kwargs: Any) -> bool:
-        response = messages[-1][-1]["content"]
+        response = [messages[-1][-1]["content"]]
         is_correct = self.verifier_func(response, answer)
         print('*************')
+        print('response', response)
+        print('answer', answer)
         print('messages', messages)
         print('count', self.count_responses(messages))
         is_final = self.count_responses(messages) >= self.max_tries
